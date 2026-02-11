5bae91bd0954a9223110c1babd6f5f2a84cfc3f0
subhadeep3902 <subha9.5roy350@gmail.com>
2026-02-11 19:23:19 +0530

    added ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { GeistPixelTriangle } from "geist/font/pixel";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,9 +16,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en" className="dark overflow-x-hidden">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${GeistSans.variable} ${GeistMono.variable} ${GeistPixelTriangle.variable}`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
+import { Header } from "@/components/shared/Header";
+import Separator from "../components/shared/Separator";
+import { Panel, PanelTitle, PanelTitleSup } from "@/components/shared/Panel";
+
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="max-w-4xl mx-auto min-h-screen">
+      <Panel>
+      <Header />
+      </Panel>
+
+      <Separator />
+
+      <Panel>
+        <PanelTitle>
+          Welcome to the Interview Kit!
+        </PanelTitle>
+
+        <PanelTitleSup>
+          Your one-stop solution for acing technical interviews.
+        </PanelTitleSup>
+      </Panel>
+
+      <Separator />
+
     </div>
   );
 }
